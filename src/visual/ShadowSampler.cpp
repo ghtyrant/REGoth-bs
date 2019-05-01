0fef17533931a550908f85fc9850ec398a4635d9
Markus Worchel <mworchel@hotmail.de>
2019-05-01 13:02:45 +0200

    Implement extraction of brightness per vertex

--- src/visual/ShadowSampler.cpp
+++ src/visual/ShadowSampler.cpp
@@ -2,13 +2,20 @@
 #include <Components/BsCMeshCollider.h>
 #include <Components/BsCRenderable.h>
 #include <Math/BsRay.h>
+#include <Mesh/BsMesh.h>
+#include <Physics/BsPhysicsMesh.h>
+#include <RenderAPI/BsVertexDataDesc.h>
 #include <Scene/BsSceneObject.h>
 
 namespace REGoth
 {
-  ShadowSampler::ShadowSampler(bs::HMeshCollider collider)
-      : m_collider(collider)
+  ShadowSampler::ShadowSampler(const bs::HMesh& targetMesh, const bs::HMeshCollider& targetCollider)
   {
+    doSanityChecks(targetMesh, targetCollider);
+
+    m_targetCollider = targetCollider;
+
+    extractBrightnessPerVertex(targetMesh);
   }
 
   bool ShadowSampler::sampleFor(bs::HSceneObject so, ShadowSample& sample)
@@ -22,19 +29,80 @@ namespace REGoth
 
     // Trace a sample ray from the scene object to our associated collider
     bs::PhysicsQueryHit hit;
-    if (!m_collider->rayCast(sampleRay, hit))
+    if (!m_targetCollider->rayCast(sampleRay, hit))
     {
       return false;
     }
 
-    // Obtain the mesh
-    auto mesh = m_collider->getMesh();
-    if (!mesh)
+    auto vertexIdx = hit.unmappedTriangleIdx * 3U;
+  }
+
+  void ShadowSampler::doSanityChecks(const bs::HMesh& targetMesh,
+                                     const bs::HMeshCollider& targetCollider)
+  {
+    // Perform some sanity checks (for valid mesh data and existance of vertex colors)
+
+    if (targetMesh == nullptr || targetCollider == nullptr)
     {
       // TODO: Error
     }
 
-    //
+    auto originalMeshData = targetMesh->getCachedData();
+    auto physicsMeshData  = targetCollider->getMesh()->getMeshData();
+
+    if (originalMeshData == nullptr || physicsMeshData == nullptr)
+    {
+      // TODO: Error
+    }
+
+    auto vertexDesc         = originalMeshData->getVertexDesc();
+    auto vertexColorElement = vertexDesc->getElement(bs::VertexElementSemantic::VES_COLOR);
+
+    if (!vertexColorElement)
+    {
+      // TODO: Error
+    }
+  }
+
+  std::function<bs::Color(bs::UINT32)> ShadowSampler::getVertexColorUnpackFunction(
+      const bs::MeshData& meshData)
+  {
+    // The function to unpack vertex colors is determined by the type of the vertex color element
+
+    auto vertexColorElement =
+        meshData.getVertexDesc()->getElement(bs::VertexElementSemantic::VES_COLOR);
+
+    std::function<bs::Color(bs::UINT32)> unpackFunction = bs::Color::fromRGBA;
+    if (vertexColorElement->getType() == bs::VertexElementType::VET_COLOR_ARGB)
+    {
+      unpackFunction = bs::Color::fromARGB;
+    }
+    else if (vertexColorElement->getType() == bs::VertexElementType::VET_COLOR_ABGR)
+    {
+      unpackFunction = bs::Color::fromABGR;
+    }
+
+    return unpackFunction;
+  }
+
+  void ShadowSampler::extractBrightnessPerVertex(const bs::HMesh& targetMesh)
+  {
+    auto meshData = targetMesh->getCachedData();
+
+    bs::Vector<bs::UINT32> vertexColors(meshData->getNumVertices());
+    targetMesh->getCachedData()->getVertexData(bs::VertexElementSemantic::VES_COLOR,
+                                               vertexColors.data(),
+                                               vertexColors.size() * sizeof(bs::UINT32));
+
+    m_targetBrightnessPerVertex.reserve(vertexColors.size());
+
+    // Extract brightness per vertex as average over the color channels
+    auto unpackColor = getVertexColorUnpackFunction(*meshData);
+    for (bs::UINT32 packedColor : vertexColors)
+    {
+      bs::Color color = unpackColor(packedColor);
+      m_targetBrightnessPerVertex.push_back((color.r + color.g + color.b) / 3.f);
+    }
   }
 
   bool ShadowSampler::getSampleRay(bs::HSceneObject so, bs::Ray& ray)
--- src/visual/ShadowSampler.hpp
+++ src/visual/ShadowSampler.hpp
@@ -10,13 +10,20 @@ namespace REGoth
   class ShadowSampler
   {
   public:
-    ShadowSampler(bs::HMeshCollider collider);
+    ShadowSampler(const bs::HMesh& targetMesh, const bs::HMeshCollider& targetCollider);
 
     bool sampleFor(bs::HSceneObject so, ShadowSample& sample);
 
   private:
+    void doSanityChecks(const bs::HMesh& targetMesh, const bs::HMeshCollider& targetCollider);
+
+    std::function<bs::Color(bs::UINT32)> getVertexColorUnpackFunction(const bs::MeshData& meshData);
+
+    void extractBrightnessPerVertex(const bs::HMesh& targetMesh);
+
     bool getSampleRay(bs::HSceneObject so, bs::Ray& ray);
 
-    bs::HMeshCollider m_collider;
+    bs::HMeshCollider m_targetCollider;
+    bs::Vector<float> m_targetBrightnessPerVertex;
   };
 }  // namespace REGoth
