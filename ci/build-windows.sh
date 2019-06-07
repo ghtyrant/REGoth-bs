ef9c8c55df60e23b3aa2b32cb03d1262013c6617
Andre Taulien <a.taulien@live.de>
2019-06-07 19:16:58 +0200

    Windows: Build x64

--- ci/build-windows.sh
+++ ci/build-windows.sh
@@ -7,5 +7,5 @@ cd build
 curl -O https://dilborceisv8p.cloudfront.net/bsf_2019.06.07_win64.zip
 unzip bsf_2019.06.07_win64.zip -d bsf
 
-cmake -Dbsf_INSTALL_DIR=bsf -DCMAKE_BUILD_TYPE=RelWithDebInfo -DREGOTH_USE_SYSTEM_BSF=On ../..
+cmake -Dbsf_INSTALL_DIR=bsf -DCMAKE_BUILD_TYPE=RelWithDebInfo -DREGOTH_USE_SYSTEM_BSF=On -A x64 ../..
 cmake --build . --config RelWithDebInfo --parallel
