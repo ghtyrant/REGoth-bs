53bc8f161b77333820da7e6d8073d515a7572c48
Andre Taulien <a.taulien@live.de>
2019-06-07 19:09:16 +0200

    Use curl on windows

--- ci/build-windows.sh
+++ ci/build-windows.sh
@@ -4,7 +4,7 @@ rm -rf build
 mkdir build
 cd build
 
-wget https://dilborceisv8p.cloudfront.net/bsf_2019.06.07_win64.zip
+curl -O https://dilborceisv8p.cloudfront.net/bsf_2019.06.07_win64.zip
 unzip bsf_2019.06.07_linux64.zip -d bsf
 
 cmake -Dbsf_INSTALL_DIR=bsf -DCMAKE_BUILD_TYPE=RelWithDebInfo -DREGOTH_USE_SYSTEM_BSF=On ../..
